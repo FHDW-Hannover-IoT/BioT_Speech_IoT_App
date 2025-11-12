68a388c320233594a97e1a898c3f55f8e1de11c3
unknown <admin@NB23-729.ha.bib.de>
2025-11-12 19:33:59 +0100

    added notifications on events (currently not working)

--- app/src/main/java/com/fhdw/biot/speech/iot/SensorEvent.java
+++ app/src/main/java/com/fhdw/biot/speech/iot/SensorEvent.java
@@ -1,16 +1,28 @@
 package com.fhdw.biot.speech.iot;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import androidx.core.app.NotificationCompat;
+
 public class SensorEvent {
     private long timestamp;
     private String sensorType;
     private float value;
     private String id;
+    private Context context;
 
-    public SensorEvent(long timestamp, String sensorType, float value, String id) {
+    public SensorEvent(long timestamp, String sensorType, float value, String id, Context context) {
         this.timestamp = timestamp;
         this.sensorType = sensorType;
         this.value = value;
         this.id = id;
+        this.context = context;
+
+        // Notifocation
+        createNotification("title", "text");
     }
 
     public long getTimestamp() {
@@ -27,5 +39,27 @@ public class SensorEvent {
 
     public String getId() {
         return id;
+    }
+
+    private void createNotification(String title, String text){
+        NotificationChannel channel = new NotificationChannel("eventChannel", "eventChannel", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager notificationManager = getSystemService(this.context, NotificationManager.class);
+
+        // Create Notification Channel
+        notificationManager.createNotificationChannel(channel);
+
+        createNotification(title, text);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this.context)
+                        .setSmallIcon(R.drawable.outline_circle_notifications_24)
+                        .setContentTitle(title)
+                        .setContentText(text)
+                        .setOngoing(true)
+                        .setChannelId("eventChannel")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        var obj = mBuilder.build();
+        System.out.println(channel);
+
+        notificationManager.notify(1, obj);
     }
 }
