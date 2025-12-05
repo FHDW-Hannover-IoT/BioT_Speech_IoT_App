9b27a0dc28e4da648c58d6345e800ab56412d72c
unknown <admin@NB23-729.ha.bib.de>
2025-12-05 13:54:39 +0100

    added missing files

---
+++ app/src/main/java/database/entities/Sensor.java
@@ -0,0 +1,16 @@
+package database.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "knownSensors")
+public class Sensor {
+    @PrimaryKey(autoGenerate = true)
+    public int sensorID;
+
+    public String sensorName;
+
+    public String getSensorName(){return sensorName;}
+    public int getSensorID(){return sensorID;}
+    public Sensor setSensorName(String name){sensorName = name; return this;}
+}
