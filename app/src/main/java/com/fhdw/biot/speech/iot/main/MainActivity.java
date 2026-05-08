cb60b76a7612d176b64fe6ae9b5540508ea55bf6
KevGott <kevinszarafin@gmail.com>
2026-05-08 20:24:06 +0200

    Mabye Fix v2

--- app/src/main/java/com/fhdw/biot/speech/iot/main/MainActivity.java
+++ app/src/main/java/com/fhdw/biot/speech/iot/main/MainActivity.java
@@ -558,7 +558,7 @@ public class MainActivity extends BiotBaseActivity {
                                                 getString(R.string.toast_no_hardware),
                                                 Toast.LENGTH_LONG)
                                         .show();
-                                //startSimulator();
+                                startSimulator();
                             }
                         },
                         BuildConfig.SIMULATOR_FALLBACK_DELAY_MS);
